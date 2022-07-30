7f1f557ef74ef34cc5f0b2f50fe2256f64ff8c57
Indunil Aravinda (Creative Software) <Indunil.A@CreativeSoftware.com>
2022-07-31 04:54:31 +0530

    Login is required to create jokes

--- Controllers/JokesController.cs
+++ Controllers/JokesController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using JokesWebApp.Data;
 using JokesWebApp.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace JokesWebApp.Controllers
 {
@@ -50,6 +51,7 @@ namespace JokesWebApp.Controllers
             return View(joke);
         }
 
+        [Authorize]
         // GET: Jokes/Create
         public IActionResult Create()
         {
